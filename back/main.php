efd430de5727cf5ed6ea2af665612ac400f815b3
XimenZangief <s9671073@gmail.com>
2022-01-10 14:45:43 +0800

    前後台主頁切版

---
+++ back/main.php
@@ -0,0 +1,10 @@
+<div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+    <a href="?back.php&do=tit">網站標題管理</a>|
+    <a href="?back.php&do=go">動態文字管理</a>|
+    <a href="?back.php&do=rr">預告片海報管理</a>|
+    <a href="?back.php&do=vv">院線片管理</a>|
+    <a href="?back.php&do=order">電影訂票管理</a>
+</div>
+<div class="rb tab">
+    <h2 class="ct">請選擇所需功能</h2>
+</div>
