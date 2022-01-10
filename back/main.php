2b911cfd9169030a7bfe8b4076aaef5bdb68700d
XimenZangief <s9671073@gmail.com>
2022-01-10 15:14:10 +0800

    完成後台登入頁面(寫死)

--- back/main.php
+++ back/main.php
@@ -1,10 +1,44 @@
-<div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
-    <a href="?back.php&do=tit">網站標題管理</a>|
-    <a href="?back.php&do=go">動態文字管理</a>|
-    <a href="?back.php&do=rr">預告片海報管理</a>|
-    <a href="?back.php&do=vv">院線片管理</a>|
-    <a href="?back.php&do=order">電影訂票管理</a>
-</div>
-<div class="rb tab">
-    <h2 class="ct">請選擇所需功能</h2>
-</div>
+<?php
+session_start();
+if(!empty($_POST)){
+    if($_POST['acc']=='admin' && $_POST['pwd']=='1234'){
+        // 因為題目沒特別要求要註冊，所以直接寫死
+        $_SESSION['login']='admin';
+    }else{
+        echo "<div class='ct' style='color:red'>帳號或密碼錯誤</div>";
+    }
+}
+
+if (isset($_SESSION['login'])) {
+?>
+    <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
+        <a href="?back.php&do=tit">網站標題管理</a>|
+        <a href="?back.php&do=go">動態文字管理</a>|
+        <a href="?back.php&do=rr">預告片海報管理</a>|
+        <a href="?back.php&do=vv">院線片管理</a>|
+        <a href="?back.php&do=order">電影訂票管理</a>
+    </div>
+    <div class="rb tab">
+        <h2 class="ct">請選擇所需功能</h2>
+    </div>
+<?php
+} else {
+?>
+<form action="back.php" method="post">
+    <table class="tab">
+        <tr>
+            <td>帳號</td>
+            <td><input type="text" name="acc"></td>
+        </tr>
+        <tr>
+            <td>密碼</td>
+            <td><input type="password" name="pwd"></td>
+        </tr>
+        <tr>
+            <td><input type="submit" value="確定登入"></td>
+            <td></td>
+        </tr>
+    </table>
+</form>
+
+<?php } ?>
