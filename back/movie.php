07c1895f15279b40d50ce75f390a3cb72a228215
XimenZangief <s9671073@gmail.com>
2022-01-15 01:37:35 +0800

    改動foreach的變數名稱並新增註解 電影數量>2才可正常運作排序功能

--- back/movie.php
+++ back/movie.php
@@ -2,52 +2,52 @@
 <hr>
 <div style="overflow:auto; height:420px;">
     <?php
-    $movies = $Movie->all(" ORDER by rank");
-    // dd($movie);
-    foreach ($movies as $key => $movie) {
+    $rows = $Movie->all(" ORDER by rank");
+    // dd($rows);
+    foreach ($rows as $key => $row) {
     ?>
         <div style="display:flex; width:100%">
             <div style="width:10%">
-                <img src="img/<?= $movie['poster']; ?>" style="width:100%">
+                <img src="img/<?= $row['poster']; ?>" style="width:100%">
             </div>
             <div style="width:10%">
                 分級
-                <img src="icon/<?= $movie['level']; ?>.png">
+                <img src="icon/<?= $row['level']; ?>.png">
             </div>
             <div style="width:80%">
                 <div style="display:flex">
-                    <div style="width:33%">片名:<?= $movie['name']; ?></div>
-                    <div style="width:33%">片長:<?= $movie['length']; ?></div>
-                    <div style="width:33%">上映時間:<?= $movie['ondate']; ?></div>
+                    <div style="width:33%">片名:<?= $row['name']; ?></div>
+                    <div style="width:33%">片長:<?= $row['length']; ?></div>
+                    <div style="width:33%">上映時間:<?= $row['ondate']; ?></div>
                 </div>
                 <div style="text-align:right">
-                    <button class="show" data-id="<?= $movie['id']; ?>">
-                        <?= ($movie['sh'] == 1) ? "顯示" : "隱藏"; ?>
+                    <button class="show" data-id="<?= $row['id']; ?>">
+                        <?= ($row['sh'] == 1) ? "顯示" : "隱藏"; ?>
                     </button>
 
                     <?php
-                    // 上移/下移功能
+                    // 上移/下移功能(電影數量必須 > 2 才會正常運作)
                     if ($key == 0) {
-                        $up = $movie['id'] . "-" . $movie['id'];
+                        $up = $row['id'] . "-" . $row['id'];
                         // 直接取得rows索引值
-                        $down = $movie['id'] . "-" . $movies[$key + 1]['id'];
+                        $down = $row['id'] . "-" . $rows[$key + 1]['id'];
                     }
-                    if ($key == (count($movies) - 1)) {
-                        $down = $movie['id'] . "-" . $movie['id'];
-                        $up = $movie['id'] . "-" . $movies[$key - 1]['id'];
+                    if ($key == (count($rows) - 1)) {
+                        $down = $row['id'] . "-" . $row['id'];
+                        $up = $row['id'] . "-" . $rows[$key - 1]['id'];
                     }
-                    if ($key > 0 && $key < (count($movies) - 1)) {
-                        $up = $movie['id'] . "-" . $movies[$key - 1]['id'];
-                        $down = $movie['id'] . "-" . $movies[$key + 1]['id'];
+                    if ($key > 0 && $key < (count($rows) - 1)) {
+                        $up = $row['id'] . "-" . $rows[$key - 1]['id'];
+                        $down = $row['id'] . "-" . $rows[$key + 1]['id'];
                     }
                     ?>
 
                     <button type="button" class="sw" data-sw="<?= $up; ?>">往上</button>
                     <button type="button" class="sw" data-sw="<?= $down; ?>">往下</button>
-                    <button onclick="location.href='?do=edit_movie&id=<?= $movie['id']; ?>>'">編輯電影</button>
-                    <button onclick="del('movie',<?= $movie['id']; ?>)">刪除電影</button>
+                    <button onclick="location.href='?do=edit_movie&id=<?= $row['id']; ?>>'">編輯電影</button>
+                    <button onclick="del('movie',<?= $row['id']; ?>)">刪除電影</button>
                 </div>
-                <div>劇情介紹:<?= $movie['intro']; ?></div>
+                <div>劇情介紹:<?= $row['intro']; ?></div>
             </div>
         </div>
         <hr>
