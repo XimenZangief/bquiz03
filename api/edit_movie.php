cf48275e65fe32f1e3213eb219fd4bafa212e9bf
XimenZangief <s9671073@gmail.com>
2022-01-14 15:45:50 +0800

    編輯電影：複製add_movie.php做修改

---
+++ api/edit_movie.php
@@ -0,0 +1,30 @@
+<?php include "../base.php";
+
+// 驗證檔案是否為空
+if(!empty($_FILES['trailer']['tmp_name'])){
+    $_POST['trailer']=$_FILES['trailer']['name'];
+    move_uploaded_file($_FILES['trailer']['tmp_name'],"../img/".$_POST['trailer']);
+}
+if(!empty($_FILES['poster']['tmp_name'])){
+    $_POST['poster']=$_FILES['poster']['name'];
+    move_uploaded_file($_FILES['poster']['tmp_name'],"../img/".$_POST['poster']);
+}
+
+// 傳入的$_POST可以直接被修改
+// 原生
+// $_POST['ondate']=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
+// join("-",[]);或是implode函式
+$_POST['ondate']=join("-",[$_POST['year'],$_POST['month'],$_POST['day']]);
+$_POST['rank']=$Movie->math('max','id')+1;
+$_POST['sh']=1;
+
+// 因為多的欄位會導致資料庫寫入失敗，所以unset刪除多餘欄位
+unset($_POST['year']);
+unset($_POST['month']);
+unset($_POST['day']);
+
+// dd($_POST);
+$Movie->save($_POST);
+
+to("../back.php?do=movie");
+?>
