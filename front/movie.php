1298271bb24991562917c36a21e58e39bbef1e1d
XimenZangief <s9671073@gmail.com>
2022-01-17 09:16:09 +0800

    從main.php拆成include: 預告片介紹trailer.php 院線片清單movie.php

---
+++ front/movie.php
@@ -0,0 +1,11 @@
+<div class="half">
+    <h1>院線片清單</h1>
+    <div class="rb tab" style="width:95%;">
+        <table>
+            <tbody>
+                <tr> </tr>
+            </tbody>
+        </table>
+        <div class="ct"> </div>
+    </div>
+</div>
---
+++ front/trailer.php
@@ -0,0 +1,12 @@
+<div class="half" style="vertical-align:top;">
+    <h1>預告片介紹</h1>
+    <div class="rb tab" style="width:95%;">
+        <!-- 解題用不到id故刪除 -->
+        <div>
+            <ul class="lists">
+            </ul>
+            <ul class="controls">
+            </ul>
+        </div>
+    </div>
+</div>
