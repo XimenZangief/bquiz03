0be30ee26e96038acb253c9336ca3d3e1a76170c
XimenZangief <s9671073@gmail.com>
2022-01-17 16:30:07 +0800

    製作下方按鈕水平移動動畫

--- front/trailer.php
+++ front/trailer.php
@@ -8,7 +8,8 @@
         width: 210px;
         height: 260px;
         margin: auto;
-        overflow: hidden; 
+        overflow: hidden;
+        position: relative;
     }
 
     .lists .po {
@@ -17,9 +18,11 @@
         display: none;
     }
 
-    .po img {
+    .po img,
+    .icon img {
         width: 100%;
         border: 2px solid white;
+        position: absolute;
     }
 
     .controls {
@@ -33,12 +36,17 @@
     .icons {
         display: flex;
         width: 320px;
-        height: 90px;
+        height: 110px;
+        overflow: hidden;
+        font-size: 12px;
     }
 
     .icon {
         width: 80px;
-        height: 20px;
+        /* flex-grow 是膨脹，flex-shrink 是縮小 */
+        flex-shrink: 0;
+        padding: 5px;
+        position: relative;
     }
 
     .left {
@@ -66,7 +74,7 @@
                 <?php
                 $pos = $Poster->all(" WHERE `sh`=1 ORDER BY `rank`");
                 foreach ($pos as $key => $po) {
-                    echo "<div class='po'>";
+                    echo "<div class='po' data-ani='{$po['ani']}'>";
                     echo "<img src='img/{$po['path']}'>";
                     echo $po['name'];
                     echo "</div>";
@@ -95,4 +103,59 @@
 
 <script>
     $(".po").eq(0).show();
+    let i = 0;
+    // 獲取.po的個數
+    let all = $(".po").length;
+    let slides = setInterval(() => {
+        // console.log($(".po").eq(i).data('ani'));
+        i++;
+        if (i > all - 1) {
+            i = 0;
+        }
+        ani(i);
+    }, 1000);
+
+    function ani(n) {
+        // data-ani的值
+        let ani = $(".po").eq(n).data('ani');
+        // 目前.po中可見的物件
+        let now=$(".po:visiable");
+        // .po第n個物件
+        let next=$(".po").eq(n);
+
+        switch (ani) {
+            case '1':
+                //淡入淡出
+                now.fadeOut(1000);
+                next.fadeIn(1000);
+                break;
+            case '2':
+                // 縮放，確定縮完成再放
+                now.hide(1000,function(){
+                    next.show(1000);
+                });
+                break;
+            case '3':
+                // 滑入滑出，確定滑出完成再滑入
+                now.slideUp(1000,function(){
+                    next.slideDown(1000);
+                });
+                break;
+        }
+    }
+    let p=0;
+    $(".left, .right").on("click",function(){
+        // 判定物件是否有class="left"
+        if($(this).hasClass('left')){
+            if(p-1>=0){
+                p--;
+            }else{
+                if(p+1<=all-4){
+                    p++;
+                }
+            }
+        }
+        // .icon向右滑動p*80px，用500毫秒
+        $(".icon").animate({right:p*80},500);
+    })
 </script>
