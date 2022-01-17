e7cc1f5caa811dc91e827615c640b68f4883eb9b
XimenZangief <s9671073@gmail.com>
2022-01-17 16:38:09 +0800

    更新sql

--- web03.sql
+++ web03.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2022-01-10 07:11:03
+-- 產生時間： 2022-01-17 09:37:43
 -- 伺服器版本： 10.4.21-MariaDB
 -- PHP 版本： 7.4.24
 
@@ -42,6 +42,16 @@ CREATE TABLE `movie` (
   `sh` tinyint(1) NOT NULL DEFAULT 1 COMMENT '顯示'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
+--
+-- 傾印資料表的資料 `movie`
+--
+
+INSERT INTO `movie` (`id`, `name`, `level`, `length`, `ondate`, `publish`, `director`, `trailer`, `poster`, `intro`, `rank`, `sh`) VALUES
+(18, 'peko', 1, 0, '2022-01-17', 'peko', 'peko', 'pekoT.mp4', 'pekoP.png', 'pekopekopekopekopeko', 1, 1),
+(19, 'saka', 1, 0, '2022-01-17', 'saka', 'saka', 'sakaT.mp4', 'sakaT.gif', 'sakasakasakasaka', 19, 1),
+(20, 'zangi', 1, 0, '2022-01-17', 'zangi', 'zangi', 'zangiT.mp4', 'zangiP.png', '            zangizangizangizangi', 20, 1),
+(21, 'lucia', 1, 0, '2022-01-17', 'lucia', 'lucia', 'luciaT.mp4', 'luciaP.png', 'lucialucialucialucialucialucia', 21, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -74,6 +84,17 @@ CREATE TABLE `poster` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 
 --
+-- 傾印資料表的資料 `poster`
+--
+
+INSERT INTO `poster` (`id`, `path`, `name`, `rank`, `sh`, `ani`) VALUES
+(5, '03A02.jpg', '03A02', 2, 1, 1),
+(6, '03A03.jpg', '03A03', 3, 1, 2),
+(7, '03A04.jpg', '03A04', 4, 1, 3),
+(8, '03A05.jpg', '005', 5, 1, 2),
+(9, '03A06.jpg', '006', 6, 1, 3);
+
+--
 -- 已傾印資料表的索引
 --
 
@@ -103,7 +124,7 @@ ALTER TABLE `poster`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `movie`
 --
 ALTER TABLE `movie`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號';
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=22;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `ord`
@@ -115,7 +136,7 @@ ALTER TABLE `ord`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `poster`
 --
 ALTER TABLE `poster`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號';
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '流水號', AUTO_INCREMENT=13;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
