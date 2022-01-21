18646ad95d60c2bed3e88974f9074c59357fd100
XimenZangief <s9671073@gmail.com>
2022-01-21 15:03:05 +0800

    訂票畫面切換、建構座位表 $ss場次時間丟進base

--- front/order.php
+++ front/order.php
@@ -22,6 +22,14 @@
     .sec select {
         width: 100%;
     }
+
+    #booking{
+        width: 540px;
+        height: 370px;
+        margin: auto;
+        background: url('icon/03D04.png');
+        background-position: top;
+    }
 </style>
 
 <h3 class='ct'>線上訂票</h3>
@@ -52,19 +60,43 @@
     </div>
     <div class="row">
         <div class="ct" style="width:100%">
-            <button>確定</button>
-            <button>重置</button>
+            <button onclick="booking()">確定</button>
+            <button onclick="reset()">重置</button>
         </div>
     </div>
 </div>
 
+<div id="booking" style="display:none"></div>
 <script>
     
     // 從網址取得id的值
     let id=(new URL(location)).searchParams.get('id');
     getMovies(id);
 
+    // #movie有內容有變動，執行getDays
     $("#movie").on("change",()=>{getDays()});
+
+    function booking(){
+        $("#order, #booking").toggle();
+
+        let order={ id:$("#movie").val(),
+                    date:$("#date").val(),
+                    session:$("#session").val()
+        }
+        $.get("api/booking.php",order,(booking)=>{
+            $("#booking").html(booking);
+        })
+    }
+
+    function reset(){
+        getMovies(id);
+    }
+
+    // 回上一步，關閉#order, #booking並清空booking內容
+    function prev(){
+        $("#order, #booking").toggle();
+        $("#booking").html("");
+    }
 
     // 取得電影清單
     function getMovies(id){
