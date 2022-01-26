2e7be32b18238de888d6d30c229f217b78ac7da1
XimenZangief <s9671073@gmail.com>
2022-01-26 15:38:38 +0800

    Fixed 過日場次不能訂票

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -2,8 +2,14 @@
 include_once "../base.php";
 $movie=$Movie->find($_GET['id']);
 $date=$_GET['date'];
+// date("G")24HR格式，沒有前導零 0~23
+if($date!=date("Y-m-d") || date("G")<14){
+    $s=5;
+}else{
+    $s=5-ceil((date("G")-13)/2);
+}
 
-for($i=1;$i<=5;$i++){
+for($i=(6-$s);$i<=5;$i++){
     $seats=$Ord->math('sum','qt',['movie'=>$movie['name'],'date'=>$date,'session'=>$ss[$i]]);
     echo "<option value='$i'>{$ss[$i]} 剩餘座位".(20-$seats)." </option>";
 } 
